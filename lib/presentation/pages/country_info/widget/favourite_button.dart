c46093b9b2a5c75e3e850e2aebb2721991ba418c
CarlD-fac <carl.delrieu@univ-tlse3.fr>
2022-03-03 22:12:17 +0100

    [ADD] Add the favourite button widget

---
+++ lib/presentation/pages/country_info/widget/favourite_button.dart
@@ -0,0 +1,42 @@
+import 'package:country_info_app/domain/features/add_favourite.dart';
+import 'package:country_info_app/domain/features/delete_favourite.dart';
+import 'package:country_info_app/domain/models/country.dart';
+import 'package:country_info_app/service_locator.dart';
+import 'package:flutter/material.dart';
+
+class FavouriteButton extends StatefulWidget {
+  const FavouriteButton(this.country, {Key? key}) : super(key: key);
+
+  final Country country;
+
+  @override
+  State<FavouriteButton> createState() => _FavouriteButtonState();
+}
+
+class _FavouriteButtonState extends State<FavouriteButton> {
+  final List<bool> _selections = List.generate(1, (_) => false);
+
+  @override
+  Widget build(BuildContext context) {
+    return _favouriteButton(widget.country);
+  }
+
+  _favouriteButton(Country country) {
+    return ToggleButtons(
+        children: const [Icon(Icons.star)],
+        color: Colors.grey,
+        selectedColor: Colors.yellow,
+        fillColor: Colors.transparent,
+        renderBorder: false,
+        borderRadius: BorderRadius.circular(25.0),
+        isSelected: _selections,
+        onPressed: (int i) {
+          setState(() {
+            _selections[i] = !_selections[i];
+            (_selections[i])
+                ? locator<AddFavouriteUseCase>().execute(country)
+                : locator<DeleteFavouriteUseCase>().execute(country.id);
+          });
+        });
+  }
+}
