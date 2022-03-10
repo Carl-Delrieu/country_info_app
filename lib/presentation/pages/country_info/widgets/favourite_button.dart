048c047f4be5973b77ea1c04e39368b053829a34
Carl-Delrieu <carl.delrieu28@icloud.com>
2022-03-10 11:53:55 +0100

    [MODIFY] Modify the favourite button

--- lib/presentation/pages/country_info/widgets/favourite_button.dart
+++ lib/presentation/pages/country_info/widgets/favourite_button.dart
@@ -1,7 +1,7 @@
-import 'package:country_info_app/domain/features/add_favourite.dart';
-import 'package:country_info_app/domain/features/delete_favourite.dart';
 import 'package:country_info_app/domain/models/country.dart';
-import 'package:country_info_app/service_locator.dart';
+import 'package:country_info_app/presentation/pages/base_page.dart';
+import 'package:country_info_app/presentation/pages/country_info/country_info_scoped_model.dart';
+import 'package:country_info_app/presentation/pages/view_state.dart';
 import 'package:flutter/material.dart';
 
 class FavouriteButton extends StatefulWidget {
@@ -22,23 +22,26 @@ class _FavouriteButtonState extends State<FavouriteButton> {
 
   _favouriteButton(Country country) {
     final List<bool> _selections = List.generate(1, (_) => country.isFavourite);
-    return ToggleButtons(
-        children: const [Icon(Icons.star)],
-        color: Colors.grey,
-        selectedColor: Colors.yellow,
-        fillColor: Colors.transparent,
-        renderBorder: false,
-        borderRadius: BorderRadius.circular(25.0),
-        isSelected: _selections,
-        onPressed: (int i) {
-          setState(() {
-            _selections[i] = !_selections[i];
-            if (_selections[i]) {
-              locator<AddFavouriteUseCase>().execute(country);
-            } else {
-              locator<DeleteFavouriteUseCase>().execute(country);
-            }
-          });
+    return BasePage<CountryInfoScopedModel>(
+        initialState: ViewState.ready,
+        onScopedModelReady: (s) {
+          s.country = country;
+        },
+        builder: (context, child, scopedModel) {
+          return ToggleButtons(
+              children: const [Icon(Icons.star)],
+              color: Colors.grey,
+              selectedColor: Colors.yellow,
+              fillColor: Colors.transparent,
+              renderBorder: false,
+              borderRadius: BorderRadius.circular(25.0),
+              isSelected: _selections,
+              onPressed: (int i) {
+                setState(() {
+                  _selections[i] = !_selections[i];
+                  //scopedModel.onFavouriteButtonClick();
+                });
+              });
         });
   }
 }
