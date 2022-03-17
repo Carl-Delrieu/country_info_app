5b73e7e78f80ab8c718a0e6ae2eb1305eb8ff636
Carl Delrieu <carl.delrieu28@icloud.com>
2022-03-17 16:39:08 +0100

    [MODIFY] Changed Futures into Streams

--- lib/domain/repositories/country_repository.dart
+++ lib/domain/repositories/country_repository.dart
@@ -12,50 +12,60 @@ class CountryRepository {
   final CountryEndpoint _endpoint;
   late Connectivity _connectivity;
 
+  List<Country> _countries = [];
+
   CountryRepository(this._dao, this._endpoint) {
     _connectivity = Connectivity();
   }
 
-  Future<List<Country>> _getAllFromDatabase() async {
-    List<CountryEntity> countries = await _dao.getCountries();
-    return countries
-        .map((e) => CountryMapper.fromEntity(e).copyWith(isFavourite: true))
-        .toList();
-  }
+  Stream<List<Country>> getAllCountries() {
+    return _getAllFromDatabase()
+        .asyncMap((List<Country> favouriteCountries) async {
+      ConnectivityResult connectivityResult =
+          await _connectivity.checkConnectivity();
 
-  Future<List<Country>> getAllCountries() async {
-    List<Country> _countries;
+      final isNotMemoized = _countries.isEmpty;
+      final hasNoNetwork = connectivityResult == ConnectionState.none;
 
-    ConnectivityResult connectivityResult =
-        await _connectivity.checkConnectivity();
-
-    final hasNoNetwork = connectivityResult == ConnectionState.none;
-
-    if (hasNoNetwork) {
-      _countries = await _getAllFromDatabase();
-    } else {
-      try {
-        final dtoList = await _endpoint.getCountries();
-        List<Country> favouriteList = await _getAllFromDatabase();
-        _countries = dtoList
-                ?.map((CountryDTO e) => CountryMapper.fromDTO(e))
-                .map((country) => country.copyWith(
-                    isFavourite:
-                        favouriteList.any((fav) => fav.name == country.name)))
-                .toList() ??
-            [];
-      } catch (e) {
-        _countries = await _getAllFromDatabase();
+      if (isNotMemoized) {
+        if (hasNoNetwork) {
+          return favouriteCountries;
+        } else {
+          try {
+            final apiList = await _endpoint.getCountries();
+            _countries = apiList
+                    ?.map((CountryDTO e) => CountryMapper.fromDTO(e))
+                    .toList() ??
+                [];
+            return _combineFavListIntoApiList(
+                favList: favouriteCountries, apiList: _countries);
+          } catch (e) {
+            return favouriteCountries;
+          }
+        }
       }
-    }
 
-    return _countries;
+      return _combineFavListIntoApiList(
+          favList: favouriteCountries, apiList: _countries);
+    });
   }
 
-  Stream<List<Country>> getAllFavourites() => _dao.watchCountries().map((e) => e
-      .map((entity) =>
-          CountryMapper.fromEntity(entity).copyWith(isFavourite: true))
-      .toList());
+  List<Country> _combineFavListIntoApiList(
+      {required List<Country> favList, required List<Country> apiList}) =>
+      apiList
+          .map((country) => country.copyWith(
+          isFavourite: favList.any((fav) => fav.name == country.name)))
+          .toList();
+
+  Stream<List<Country>> getAllFavourites() => _getAllFromDatabase();
+
+  Stream<List<Country>> _getAllFromDatabase() {
+    Stream<List<CountryEntity>> countries = _dao.watchCountries();
+    return countries.map((streamedList) => streamedList
+        .map((entity) =>
+        CountryMapper.fromEntity(entity).copyWith(isFavourite: true))
+        .toList());
+  }
 
   Future<void> addCountry(Country country) async {
     country.isFavourite = true;
