17028e9b71dcd8cf62f7b2e973956006c1d65672
Carl Delrieu <carl.delrieu28@icloud.com>
2022-03-18 12:23:20 +0100

    [ADD] Adding theming files

--- lib/presentation/theming/themes.dart
+++ lib/presentation/theming/themes.dart
@@ -1,21 +1,28 @@
 import 'package:country_info_app/presentation/theming/colors.dart';
 import 'package:flutter/material.dart';
 
-ThemeData lightTheme = ThemeData(
-    brightness: Brightness.light,
+final ThemeData lightTheme = _lightThemeBuilder();
+final ThemeData darkTheme = _darkThemeBuilder();
+
+ThemeData _lightThemeBuilder() {
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
     primaryColor: lightPrimaryColor,
     floatingActionButtonTheme: const FloatingActionButtonThemeData(
-        backgroundColor: lightAccentColor,
+      backgroundColor: lightAccentColor,
     ),
     backgroundColor: lightBackgroundColor,
+  );
+}
 
-);
-
-ThemeData darkTheme = ThemeData(
+ThemeData _darkThemeBuilder() {
+  final ThemeData base = ThemeData.dark();
+  return base.copyWith(
     brightness: Brightness.dark,
     primaryColor: darkPrimaryColor,
     floatingActionButtonTheme: const FloatingActionButtonThemeData(
-        backgroundColor: darkAccentColor,
+      backgroundColor: darkAccentColor,
     ),
     backgroundColor: darkBackgroundColor,
-);
+  );
+}
