10fa05a95b6bf48fc5ce39a9e7d503cd5847fd09
Carl Delrieu <carl.delrieu28@icloud.com>
2022-03-21 20:06:03 +0100

    [ADD] Adding tDbName

--- lib/data/local/db_strings.dart
+++ lib/data/local/db_strings.dart
@@ -1,14 +1,5 @@
 class DBStrings {
   static const dbName = 'country_database.sqlite';
+  static const tDbName = 'country_test_database.sqlite';
   static const tableName = 'CountryEntity';
-  static const id = 'id';
-  static const name = 'name';
-  static const abbreviation = 'abbreviation';
-  static const capital = 'capital';
-  static const currency = 'currency';
-  static const phone = 'phone';
-  static const population = 'population';
-  static const flag = 'flag';
-  static const emblem = 'emblem';
-  static const orthographic = 'orthographic';
 }
