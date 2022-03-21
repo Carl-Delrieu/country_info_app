ac54891f77950a615e61050544a6b9807564a919
Carl Delrieu <carl.delrieu28@icloud.com>
2022-03-21 20:05:18 +0100

    [ADD] Add fake API service

--- lib/data/remote/endpoints/fake_api_service.dart
+++ lib/data/remote/endpoints/fake_api_service.dart
@@ -8,22 +8,25 @@ import 'package:mockito/mockito.dart';
 class FakeAPIService extends APIService {
   FakeAPIService(http.Client client) : super(client);
 
-  static const _baseURL = APIStrings.baseURL;
+  static const _baseUrl = APIStrings.baseURL;
   static const _endpoint = APIStrings.endpoint;
-  static const _successfulResponse = '''{
-    "abbreviation": "TS",
-    "capital": "Test-City",
-    "currency": "TTC",
-    "name": "Test-Land",
-    "phone": "2051920",
-    "population": 2,
-    "media": {
-      "flag": "https://www.pinclipart.com/picdir/middle/194-1949582_science-test-svg-icon-science-clipart.png",
-      "emblem": "",
-      "orthographic": ""
-    },
-    "id": 30094000
-  }''';
+  static const _successfulResponse = '''
+  [
+    {
+      "abbreviation": "TST",
+      "capital": "Test-City",
+      "currency": "TTC",
+      "name": "Test-Land",
+      "phone": "2051920",
+      "population": 2,
+      "media": {
+        "flag": "https://www.pinclipart.com/picdir/middle/194-1949582_science-test-svg-icon-science-clipart.png",
+        "emblem": "",
+        "orthographic": ""
+      },
+      "id": 30094000
+    }
+  ]''';
   static const _failingResponse = '''{
     "message": "The requested URL was not found on the server. If you entered the URL manually please check your spelling and try again."
   }''';
@@ -31,10 +34,12 @@ class FakeAPIService extends APIService {
   factory FakeAPIService.getInstance() {
     final client = MockClient();
 
-    when(client.get(Uri.https(_baseURL, _endpoint)))
+    when(client.get(Uri.https(_baseUrl, _endpoint),
+            headers: anyNamed('headers')))
         .thenAnswer((_) async => http.Response(_successfulResponse, 200));
 
-    when(client.get(argThat(isNot(Uri.https(_baseURL, _endpoint)))))
+    when(client.get(argThat(isNot(Uri.https(_baseUrl, _endpoint))),
+            headers: anyNamed('headers')))
         .thenAnswer((_) async => http.Response(_failingResponse, 404));
 
     return FakeAPIService(client);
